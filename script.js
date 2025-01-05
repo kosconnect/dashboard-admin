0c1480e26ec9815aab1299dae17ae5552ad76915
balqisrosa <balqisrosasekamayang@gmail.com>
2025-01-05 18:34:09 +0700

    modify dashboard admin

--- script.js
+++ script.js
@@ -3,3 +3,45 @@ function toggleDropdown() {
     // Toggle the display between none and block
     dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
 }
+
+//Function show POP UP
+function showPopup() {
+    document.getElementById('popupTambahFasilitas').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popupTambahFasilitas').style.display = 'none';
+}
+
+document.getElementById('formTambahFasilitas').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Fasilitas berhasil ditambahkan!');
+    closePopup();
+});
+
+function showPopupEdit() {
+    document.getElementById('popupEditFasilitas').style.display = 'block';
+}
+
+function showPopupDelete() {
+    document.getElementById('popupHapusFasilitas').style.display = 'block';
+}
+
+function closePopup() {
+    document.querySelectorAll('.popup').forEach(popup => popup.style.display = 'none');
+}
+
+function confirmDelete() {
+    alert('Fasilitas kamar berhasil dihapus!'); // Ganti dengan fungsi penghapusan yang sesuai.
+    closePopup();
+}
+
+function showPopupUbahRole(userName, currentRole) {
+    document.getElementById('popupUbahRoleUser').style.display = 'block';
+    document.getElementById('userName').value = userName;
+    document.getElementById('userRole').value = currentRole;
+}
+
+function closePopup() {
+    document.querySelectorAll('.popup').forEach(popup => popup.style.display = 'none');
+}
