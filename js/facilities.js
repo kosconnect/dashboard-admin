792089d6b2c09001c46bb4b89a48adf7defc2c34
balqisrosa <balqisrosasekamayang@gmail.com>
2025-01-21 21:26:55 +0700

    post fasilitas pt1

--- js/facilities.js
+++ js/facilities.js
@@ -102,7 +102,7 @@ document.getElementById('formTambahFasilitas').addEventListener('submit', functi
     };
 
     // Kirim permintaan POST ke API
-    fetch('https://kosconnect-server.vercel.app/api/facilities/', {
+    fetch('https://kosconnect-server.vercel.app/api/facilities', {
         method: 'POST',
         headers: {
             'Authorization': `Bearer ${jwtToken}`,
